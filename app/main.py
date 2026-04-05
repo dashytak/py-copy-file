65f2b51a478e5d85c09fe4f703098caba2a74531
vladiukdaria <vladiukdaria@gmail.com>
2026-04-05 21:00:14 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
 # write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    if parts[0] != "cp":
+        return
+
+    source = parts[1]
+    destination = parts[2]
+
+    if source == destination:
+        return
+
+    try:
+        with open(source, "r") as file_in, open(destination, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        return
